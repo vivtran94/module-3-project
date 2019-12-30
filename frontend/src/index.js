393b160840469cc9ce013f8560a16526b37675b0
Nbuck3905 <Nathanbuchanan671@gmail.com>
2019-12-30 15:04:27 -0600

    Updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,25 +1,64 @@
-let play = document.querySelector(`#Play`)
-let create = document.querySelector(`#Create`)
-
-
-play.addEventListener("click", function(){
-    console.log("Click")
-    document.body.innerText = ""
-    let createUser = document.createElement("form")
-    createUser.innerText = "User-Name"
-    let inputTag = document.createElement("input")
-    document.body.setAttribute("id","name")
+document.addEventListener("DOMContentLoaded",function(){
+    let welcome = document.createElement("h1")
+        welcome.innerText = "Welcome"
+    let formDiv = document.createElement("div")
+        formDiv.setAttribute("class","login-page")
+    let div = document.createElement("div")
+        div.setAttribute("class","form")
+    let loginForm = document.createElement("form")
+        loginForm.setAttribute("class","login-form")
+    let input = document.createElement("input")
+        input.type = "text"
+        input.placeholder = "User Name"
     let playButton = document.createElement("button")
-    playButton.setAttribute("id","Play1")
-    playButton.innerText = "Play"
-    document.body.append(createUser)
-    createUser.append(inputTag,playButton)
+        playButton.innerText = "Play"
+        playButton.id = "play"
+    let register = document.createElement("p")
+        register.setAttribute("class", "message")
+        register.innerText = "Not Registered ?"
+    let createAccount = document.createElement("a")
+        createAccount.setAttribute("id", "create")
+        createAccount.innerText = "  Create Account" 
+        createAccount.href = "#"  
+        
+        document.body.append(welcome,formDiv)
+        formDiv.append(div)
+        div.append(loginForm)
+        register.append(createAccount)
+        loginForm.append(input,playButton,register)
+        
+        playButton.addEventListener("click", function(){
+            console.log("Click")
+        
+        })
+        
+        createAccount.addEventListener("click", function(){
+            loginForm = document.querySelector(".form")
+            loginForm.remove()
+            // document.body.innerText = ""
+            let createUserDiv = document.createElement("div")
+            createUserDiv.setAttribute("class", "form")
+            let createUserForm = document.createElement("form")
+            createUserForm.setAttribute("class", "login-form")
+            let inputTag = document.createElement("input")
+            inputTag.type = "text"
+            inputTag.placeholder = "username"
+            let playButton_1 = document.createElement("button")
+            playButton_1.setAttribute("id","play1")
+            playButton_1.innerText = "Play"
+            document.body.append(createUserDiv)
+            createUserDiv.append(createUserForm)
+            createUserForm.append(inputTag, playButton_1)
+            
+        playButton_1.addEventListener("click", function(e){
+            e.preventDefault()
+            console.log("I was clicked")
+            
+        
+            })
+        })
 
-    let playButton1 = document.querySelector(`#Play1`)
-    playButton1.addEventListener("click", function(e){
-        e.preventDefault()
-        console.log("I was clicked")
 
-    })
+
 })
 
